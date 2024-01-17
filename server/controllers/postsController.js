ea7adb8198f9482262112f301a48a11b65bcc36a
Valentina <128539436+vnasp@users.noreply.github.com>
2024-01-17 00:32:58 -0300

    Update postsController.js

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -5,8 +5,16 @@ const getAllPosts = async (req, res) => {
     const posts = await getPosts()
     res.status(200).json(posts)
   } catch (error) {
-    res.status(500).json({ error: "Error al procesar la solicitud" })
-    console.error("Error al procesar la solicitud:", error)
+    const { code } = error
+    if (code == "23502") {
+      res.status(400)
+        .send("Se ha violado la restricción NOT NULL en uno de los campos de la tabla")
+    } else if (code == "22P02") {
+      res.status(400)
+        .send("Bad Request: el servidor no puede procesar la solicitud debido a un error del cliente")
+      } else {
+      res.status(500).send(error)
+    }
   }
 }
 
@@ -16,8 +24,16 @@ const addPosts = async (req, res) => {
       const newPost = await addPost(titulo, imgSrc, descripcion)
       res.status(201).json(newPost)
   } catch (error) {
-    res.status(500).json({ error: "Error al procesar la solicitud" })
-    console.error("Error al procesar la solicitud:", error)
+    const { code } = error
+    if (code == "23502") {
+      res.status(400)
+        .send("Se ha violado la restricción NOT NULL en uno de los campos de la tabla")
+    } else if (code == "22P02") {
+      res.status(400)
+        .send("Bad Request: el servidor no puede procesar la solicitud debido a un error del cliente")
+      } else {
+      res.status(500).send(error)
+    }
   }
 }
 
@@ -28,8 +44,16 @@ const updatePosts = async (req, res) => {
     await updatePost(likes, id)
     res.status(201).send("Fotografía con nuevo like")
   } catch (error) {
-    res.status(500).json({ error: "Error al procesar la solicitud" })
-    console.error("Error al procesar la solicitud:", error)
+    const { code } = error
+    if (code == "23502") {
+      res.status(400)
+        .send("Se ha violado la restricción NOT NULL en uno de los campos de la tabla")
+    } else if (code == "22P02") {
+      res.status(400)
+        .send("Bad Request: el servidor no puede procesar la solicitud debido a un error del cliente")
+      } else {
+      res.status(500).send(error)
+    }
   }
 }
 
@@ -38,9 +62,17 @@ const deletePosts = async (req, res) => {
     const { id } = req.params
     await deletePost(id)
     res.status(201).send("Fotografía eliminada con éxito")
-  } catch (error) {
-    res.status(500).json({ error: "Error al procesar la solicitud" })
-    console.error("Error al procesar la solicitud:", error)
+ } catch (error) {
+    const { code } = error
+    if (code == "23502") {
+      res.status(400)
+        .send("Se ha violado la restricción NOT NULL en uno de los campos de la tabla")
+    } else if (code == "22P02") {
+      res.status(400)
+        .send("Bad Request: el servidor no puede procesar la solicitud debido a un error del cliente")
+      } else {
+      res.status(500).send(error)
+    }
   }
 }
 
